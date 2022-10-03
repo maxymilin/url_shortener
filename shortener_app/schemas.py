6afaf647af0171ff1f742dfa698f5c5429fa8e72
Maxym Ilin <maxym.ilin@uvik.net>
2022-10-03 22:05:40 +0300

    Create Pydatinc models

---
+++ shortener_app/schemas.py
@@ -0,0 +1,19 @@
+from calendar import c
+from pydantic import BaseModel
+
+
+class URLBase(BaseModel):
+    """URL base class what has target_url.
+    Contains 'long' urls form users.
+    """
+    target_url: str
+
+
+class URL(URLBase):
+    """URL with chortered url and user ip."""
+    url: str
+    user_ip: str
+
+    class Config:
+        """Tell pydantic to work with a database model"""
+        orm_mode = True
