1c27e6080936c346221226033518f2c7208181bd
Maxym Ilin <maxym.ilin@uvik.net>
2022-10-04 22:33:55 +0300

    Working base version of async app.

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,24 +1,28 @@
 import secrets
 import validators
 
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 
-from sqlalchemy.orm import Session
 
+from .database import db
 from . import models, schemas
-from .database import async_session, Base, engine
-from .models_dal import UrlDAL
+
+
+db.init()
 
 app = FastAPI()
 
 
 @app.on_event("startup")
 async def startup():
-    # create db tables
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+    await db.create_all()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.close()
+
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
@@ -36,30 +40,20 @@ async def root():
 
 @app.post("/shorten_url", response_model=schemas.URL)
 async def create_url(url: schemas.URLBase):
-    async with async_session() as session:
-        async with session.begin():
-            if not validators.url(url.target_url):
-                raise_bad_request(message="URL is not valid")
-            print("I get url", url.target_url)
-            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-            key = "".join(secrets.choice(chars) for _ in range(5))
-            db_url = models.URL(
-                target_url=url.target_url, key=key
-            )
-            url_dal = UrlDAL(session)
-            await url_dal.create_url(db_url)
-            db_url.url = key
-            return db_url
+    if not validators.url(url.target_url):
+        raise_bad_request(message="URL is not valid")
+    print("I get url", url.target_url)
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    db_url = await models.URL.create(target_url=url.target_url, key=key)
+    db_url.url = key
+    return db_url
 
 
-# @app.get("/{url_key}")
-# def forvard_to_target_url(
-#     url_key: str, request: Request, db: Session = Depends(get_db)
-# ):
-#     db_url = (
-#         db.query(models.URL).filter(models.URL.key == url_key).first()
-#     )
-#     if db_url:
-#         return RedirectResponse(db_url.target_url)
-#     else:
-#         raise_not_found(request)
+@app.get("/{url_key}")
+async def forvard_to_target_url(url_key: str, request: Request):
+    db_url = await models.URL.get(url_key)
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.future import select
 
 
-from .database import Base
+from .database import Base, db
 
 
 class URL(Base):
@@ -12,3 +13,26 @@ class URL(Base):
     id = Column(Integer, primary_key=True)
     key = Column(String, unique=True, index=True)
     target_url = Column(String, index=True)
+
+    @classmethod
+    async def create(cls, key, target_url):
+        db_url = cls(key=key, target_url=target_url)
+        print(db_url)
+        db.add(db_url)
+
+        try:
+            await db.commit()
+        except Exception as e:
+            await db.rollback()
+            raise e
+        return db_url
+
+    @classmethod
+    async def get(cls, url):
+        query = select(cls).where(cls.key == url)
+        full_urls = await db.execute(query)
+        try:
+            (full_url,) = full_urls.first()
+            return full_url
+        except TypeError:
+            return None
