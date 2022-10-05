abe7ba1dcbbd9ec719efc0a4017bf40a9e770528
Maxym Ilin <maxym.ilin@uvik.net>
2022-10-05 13:32:23 +0300

    Create a new table that creates a client account and then create a table with a URL and shorter identification.

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -39,18 +39,29 @@ async def root():
 
 
 @app.post("/shorten_url", response_model=schemas.URL)
-async def create_url(url: schemas.URLBase):
+async def create_url(url: schemas.URLBase, request: Request):
+    client_ip = request.client.host + ":" + str(request.client.port) + "|"
     if not validators.url(url.target_url):
         raise_bad_request(message="URL is not valid")
-    exist_url = await models.URL.is_exist(url.target_url)
-    if exist_url:
-        update_calls = exist_url.calls_count + 1
-        await models.URL.update_calls(url=url.target_url, calls_count=update_calls)
-        return exist_url
-
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    db_url = await models.URL.create(target_url=url.target_url, key=key)
+    exist_client = await models.Client.is_exist(client_ip, url.target_url)
+    if exist_client:
+        db_url_id = exist_client.url_id
+        db_url = await models.URL.get(db_url_id)
+        key = db_url.key
+    else:
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        key = "".join(secrets.choice(chars) for _ in range(5))
+        db_url = await models.Client.create(
+            client_ip=client_ip,
+            target_url=url.target_url,
+            key=key,
+        )
+    # resp_url = schemas.URL()
+    # print(type(url.target_url))
+    # print(type(key))
+    # resp_url.target_url = url.target_url
+    # resp_url.key = key
+    # return resp_url
     return db_url
 
 
@@ -60,9 +71,15 @@ async def get_url_count():
     return f"Number of shortened urls: {count}"
 
 
+@app.get("/top10")
+async def get_top_ten():
+    result = await models.URL.get_top()
+    return result
+
+
 @app.get("/{url_key}")
 async def forvard_target_url(url_key: str, request: Request):
-    db_url = await models.URL.get(url_key)
+    db_url = await models.URL.get_full_url(url_key)
     if db_url:
         return RedirectResponse(db_url.target_url)
     else:
--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,6 +1,5 @@
-from itertools import count
-
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy import update as sqlalchemy_update
 from sqlalchemy.future import select
 
@@ -32,7 +31,17 @@ class URL(Base):
         return db_url
 
     @classmethod
-    async def get(cls, url):
+    async def get(cls, url_id):
+        query = select(cls).where(cls.id == url_id)
+        full_urls = await db.execute(query)
+        try:
+            (full_url,) = full_urls.first()
+            return full_url
+        except TypeError:
+            return None
+
+    @classmethod
+    async def get_full_url(cls, url):
         query = select(cls).where(cls.key == url)
         full_urls = await db.execute(query)
         try:
@@ -59,16 +68,63 @@ class URL(Base):
         return num_calls
 
     @classmethod
-    async def update_calls(cls, url, calls_count):
+    async def update_calls(cls, url_id, calls):
         query = (
             sqlalchemy_update(cls)
-            .where(cls.target_url == url)
-            .values(calls_count=calls_count)
+            .where(cls.id == url_id)
+            .values(calls_count=calls)
             .execution_options(synchronize_session="fetch")
         )
         await db.execute(query)
         try:
             await db.commit()
-        except Exception:
+        except Exception as e:
             await db.rollback()
-            raise
+            raise e
+
+    @classmethod
+    async def get_top(cls, limit=10):
+        query = select(cls.target_url).order_by(cls.calls_count).limit(limit)
+        top_result = await db.execute(query)
+        top_result = top_result.scalars().all()
+        return top_result
+
+
+class Client(Base):
+    """Table with unic URLs and count calls"""
+
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True)
+    client_ip_url = Column(String, unique=True, index=True)
+    url_id = Column(Integer, ForeignKey("urls.id"))
+
+    @classmethod
+    async def create(cls, client_ip, target_url, key):
+        client_ip_url = client_ip + target_url
+        db_url = await URL.is_exist(target_url)
+        if db_url:
+            url_id = db_url.id
+            print("There are url", db_url.target_url)
+            calls = db_url.calls_count + 1
+            await db_url.update_calls(url_id, calls)
+        else:
+            db_url = await URL.create(target_url=target_url, key=key)
+        db_client = cls(client_ip_url=client_ip_url, url_id=db_url.id)
+        db.add(db_client)
+        try:
+            await db.commit()
+        except Exception as e:
+            await db.rollback()
+            raise e
+        return db_url
+
+    @classmethod
+    async def is_exist(cls, client_ip, target_url):
+        query = select(cls).where(cls.client_ip_url == client_ip + target_url)
+        db_client = await db.execute(query)
+        try:
+            (full_url,) = db_client.first()
+            return full_url
+        except TypeError:
+            return False
