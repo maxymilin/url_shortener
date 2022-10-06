c998866cb13c364a3714c48572e91231dfa29416
Maxym Ilin <maxym.ilin@uvik.net>
2022-10-06 11:51:59 +0300

    Version with Docker

--- backend/shortener_app/main.py
+++ backend/shortener_app/main.py
@@ -30,6 +30,7 @@ def raise_not_found(request):
     raise HTTPException(status_code=404, detail=message)
 
 
+
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
