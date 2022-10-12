d538c5cd4fa04eaa7443cea2b05e2f3e66c96164
Maxym Ilin <maxym.ilin@uvik.net>
2022-10-12 23:04:31 +0300

    Fefactor create_url function. Split for smaller part for better testing

--- backend/shortener_app/main.py
+++ backend/shortener_app/main.py
@@ -30,34 +30,48 @@ def raise_not_found(request):
     raise HTTPException(status_code=404, detail=message)
 
 
-
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
 
-def raise_not_found(request):
-    massage = f"URL {request.url} doesn't exist."
-    raise HTTPException(status_code=404, detail=massage)
-
-
 @app.get("/")
 async def root():
-    return "Hello, World"
+    return {"message": "Hello, World"}
 
 
-@app.post("/shorten_url", response_model=schemas.URL)
-async def create_url(url: schemas.URLBase, request: Request):
+@app.get("/count")
+async def get_url_count():
+    count = await models.URL.calls()
+    return {"message": f"Number of shortened urls: {count}"}
+
+
+@app.get("/top10")
+async def get_top_ten():
+    result = await models.URL.get_top()
+    return {"top_10_url": result}
+
+
+async def get_client(url, client_ip):
+    client = await models.Client.is_exist(client_ip, url.target_url)
+    if client:
+        return client
+    return None
+
+
+async def get_key(lenght=5):
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    key = "".join(secrets.choice(chars) for _ in range(lenght))
+    return key
+
+
+async def get_url(url, request):
     client_ip = request.client.host
-    if not validators.url(url.target_url):
-        raise_bad_request(message="URL is not valid")
-    exist_client = await models.Client.is_exist(client_ip, url.target_url)
+    exist_client = await get_client(client_ip, url)
     if exist_client:
         db_url_id = exist_client.url_id
         db_url = await models.URL.get(db_url_id)
-        key = db_url.key
     else:
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        key = "".join(secrets.choice(chars) for _ in range(5))
+        key = await get_key()
         db_url = await models.Client.create(
             client_ip=client_ip,
             target_url=url.target_url,
@@ -66,17 +80,12 @@ async def create_url(url: schemas.URLBase, request: Request):
     return db_url
 
 
-
-@app.get("/count")
-async def get_url_count():
-    count = await models.URL.calls()
-    return f"Number of shortened urls: {count}"
-
-
-@app.get("/top10")
-async def get_top_ten():
-    result = await models.URL.get_top()
-    return result
+@app.post("/shorten_url", response_model=schemas.URL)
+async def create_url(url: schemas.URLBase, request: Request):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="URL is not valid")
+    db_url = await get_url(url, request)
+    return db_url
 
 
 @app.get("/{url_key}")
